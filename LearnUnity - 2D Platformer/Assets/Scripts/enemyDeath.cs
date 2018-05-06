e0339355d953bdd288738ccfdc7bd593ab1c87b0
Slavka PexHymblu <pexhymblu@gmail.com>
2018-05-06 11:39:06 +0300

    Init Push.

---
+++ LearnUnity - 2D Platformer/Assets/Scripts/enemyDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDeath : MonoBehaviour {
+
+    public GameObject enemyDeathPrefab;
+    private AudioSource enemyDeathAudio;
+    private GameObject instantiateObject;
+    public float time;
+
+    void Awake()
+    {
+        enemyDeathAudio = GameObject.FindGameObjectWithTag("enemyDeathAudio").GetComponent<AudioSource>();
+    }
+
+    public void enemyDeathInstantinate(Vector3 position)
+    {
+        enemyDeathAudio.Play();
+        instantiateObject = Instantiate(enemyDeathPrefab, position, Quaternion.identity);
+        Destroy(instantiateObject, time);
+    }
+}
---
+++ LearnUnity - 2D Platformer/Assets/Scripts/itemPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPickup : MonoBehaviour {
+
+    public GameObject itemPickupPrefab;
+    private AudioSource itemPickupAudio;
+    private GameObject instantiateObject;
+    public float time;
+
+    void Awake()
+    {
+        itemPickupAudio = GameObject.FindGameObjectWithTag("itemPickupAudio").GetComponent<AudioSource>();
+    }
+            
+    public void itemPickupInstantinate (Vector3 position)
+    {
+        itemPickupAudio.Play();
+        instantiateObject = Instantiate(itemPickupPrefab, position, Quaternion.identity);
+        Destroy(instantiateObject, time);
+    }
+}
